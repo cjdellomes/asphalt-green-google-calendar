be9cf5246909ca7416d2c1c33c49134c1d3ab428
cjdellomes <cjdellomes@gmail.com>
2024-10-21 17:09:06 -0400

    Flatten and trim scraper results
    We don't need the headers of the schedule table (i.e. the days of the
    week) since each entry in the data list already contains the necessary
    information. We also want to flatten and trim the list so it'll be
    easier for the eventual formatter to iterate.

--- scraper.py
+++ scraper.py
@@ -11,15 +11,12 @@ div_content = soup.find('div', class_=schedule_table_div)
 table = div_content.find('table')
 
 data = []
-headers = []
-
-header_row = table.find('tr')
-headers = [th.get_text(strip=True) for th in header_row.find_all('th')]
 
 for row in table.find_all('tr')[1:]:  # skip the header row
     columns = row.find_all('td')
     row_data = [col.get_text(' ', strip=True) for col in columns]
     data.append(row_data)
 
-print(headers)
-print(data)
+# flatten list of lists down into a single list and remove empty strings
+data = [item for sublist in data for item in sublist if item]
+
