44309e2d0a910f39bab9177af0292f1190873737
cjdellomes <cjdellomes@gmail.com>
2024-10-21 19:37:01 -0400

    Add schedule time to iso 8601 formatting
    We need to parse the datetime blocks and format them into ISO 8601
    format so that we can turn them into event objects for emitting to the
    calendar. The event objects take the start and end datetimes as ISO 8601
    with the timezone always set to New York.

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
 
 url = 'https://www.asphaltgreen.org/ues/schedules/field-schedule?show=1'
@@ -20,3 +21,30 @@ for row in table.find_all('tr')[1:]:  # skip the header row
 # flatten list of lists down into a single list and remove empty strings
 data = [item for sublist in data for item in sublist if item]
 
+for item in data:
+    if 'No Public Field Hours' in item:
+        continue
+    tokens = item.split()
+    tokens = [token for token in tokens if '(' not in token and ')' not in token]
+    month = tokens[0]
+    day = tokens[1]
+    for time_range in tokens[2:]:
+        start_time, end_time = time_range.split('-')
+        formats = ['%B %d %I%p %Y', '%B %d %I:%M%p %Y']  # Hour only and Hour:Minute formats
+
+        start_datetime_str = f'{month} {day} {start_time} {datetime.now().year}'
+        start_iso = None
+        for fmt in formats:
+            try:
+                start_iso = datetime.strptime(start_datetime_str, fmt).isoformat() + '-07:00'
+            except ValueError:
+                pass
+
+        end_datetime_str = f'{month} {day} {end_time} {datetime.now().year}'
+        end_iso = None
+        for fmt in formats:
+            try:
+                end_iso = datetime.strptime(end_datetime_str, fmt).isoformat() + '-07:00'
+            except ValueError:
+                pass
+        print(start_iso + ' ' + end_iso)
